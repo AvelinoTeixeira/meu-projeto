05d31442b1f606c87bd80d1b517e891c6c3ed9cc
AvelinoTeixeira <129909440+AvelinoTeixeira@users.noreply.github.com>
2023-08-05 21:20:22 +0100

    HelloWorld

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import HelloWorld from './components/HelloWorld'
 
 function App() {
+  const name = 'Avelino'
+
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Alterando JSX</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {2 + 2}</p>
+      <img src={url} alt="Minha Imgem"/>
+      <HelloWorld/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
